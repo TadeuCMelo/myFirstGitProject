a2c22b9196c84335de51f1ac95d0f83db02abc05
TadeuCMelo <tadeumelo129@gmail.com>
2024-10-14 15:39:10 +0000

    alterei o ficheiro

---
+++ teste.cpp
@@ -0,0 +1,10 @@
+// Programa básico de c++ para testar o funcionamente do computador
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
