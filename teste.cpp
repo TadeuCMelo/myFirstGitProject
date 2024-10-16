c331ce30961676b00a7d0042d515818bb5e8f848
TadeuCMelo <tadeumelo129@gmail.com>
2024-10-16 10:39:27 +0000

    alterei denovo

--- teste.cpp
+++ teste.cpp
@@ -8,3 +8,5 @@ int main() {
     cout << "Hello World!" << endl;
     return 0;
 }
+/*int é uma função ,,main é a função principal a chaveta serve para criar um bloco de código e pertence sempre há função nesse caso há função main
+o que tem antes dos parenteses é uma função sempre e o que tiver dentro dos parenteses também*/ 
