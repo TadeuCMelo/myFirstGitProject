1e8edd5ae15520304373ed1930afa5669b61176d
TadeuCMelo <tadeumelo129@gmail.com>
2024-10-17 10:36:13 +0000

    adw

---
+++ teste2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // 2 * pi * r
+    const float PI= 3.1416; //constantes nao podem ser mudados os valores e são sempre maiúsculas
+    const int X= 2;
+    
+    float valor;
+
+    cout << "indique o valor do raio:";
+    cin >> valor; //cin vai dar um output
+
+    cout << X * PI * 5 << endl;
+
+    
+}
