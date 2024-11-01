cd5818dc742e7a357230c31ac87e036fd746401b
TadeuCMelo <tadeumelo129@gmail.com>
2024-11-01 09:52:14 -0100

    Update tpc.cpp

--- tpc.cpp
+++ tpc.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-    int numeros[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    cout << "Os 20 primeiros números pares são: ";
+    int numeros[20] = {0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
     for (int i = 0 ; i < 20 ; i++ ){
         if(numeros[i] %2 == 0)
         cout << numeros[i] << " ";
