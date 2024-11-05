af40a8af7c4ed2522c82d2ab5b29b20301f796c7
TadeuCMelo <tadeumelo129@gmail.com>
2024-11-05 11:54:29 -0100

    Update tpc.cpp

--- tpc.cpp
+++ tpc.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
+#include <iostream> // inclui a biblioteca de entrada e saida padrao permitindo o cout e cin
 
-using namespace std;
+using namespace std; // permite usar elementos do namespace std para não ficar pondo atrás do código
 
-int main()
+int main() // função principal do programa
 {
-    cout << "Os 20 primeiros números pares são: ";
-    int numeros[20] = {0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-    for (int i = 0 ; i < 20 ; i++ ){
-        if(numeros[i] %2 == 0)
+    int numeros[20] = {0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}; // fazendo um array de 20 valores
+    for (int i = 0 ; i < 20 ; i++ ){ // criando um loop
+        if(numeros[i] %2 == 0 && numeros[i] != 0)
         cout << numeros[i] << " ";
     }
 
