cba0c3426ed3cfc99e9a6536744a4ebe4d07d970
TadeuCMelo <tadeumelo129@gmail.com>
2024-11-05 16:29:38 -0100

    algo

---
+++ rapido.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+    //int numeros[20] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+const int X = 10;
+int myArr[20];
+int main(){
+  // int x = 0;
+//while(x < 20) {
+  //     if(numeros[x] %2 == 0 && numeros[x] != 0){
+    //    cout <<"o número " << numeros[x] << " é par" << endl;
+      //}else{
+        //cout << "o número " << numeros[x] << " é impar" << endl;
+      //}
+      //x++;
+//}
+    
+ 
+    /*for(int i = 0 ; i < 20 ; i++){ // iteração é um loop
+      if(numeros[i] %2 == 0 && numeros[i] != 0){
+        cout <<"o número " << numeros[i] << " é par" << endl;
+      }else{
+        cout << "o número " << numeros[i] << " é impar" << endl;
+      }
+
+
+    }
+   */
+
+
+
+string xpto = "Nathan";
+string olá = "olá";
+
+myArr[10] = 999;
+
+for(int x = 0; x <= 20; x++){
+    myArr[x] = x * 2;
+    
+}
+
+
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+     cout << pares[i] << " ";
