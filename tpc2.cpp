fb51dc346a3154acf0e42b5f7ec9beca8058b10a
TadeuCMelo <tadeumelo129@gmail.com>
2024-11-10 14:40:44 -0100

    Update tpc2.cpp

--- tpc2.cpp
+++ tpc2.cpp
@@ -1,7 +1,8 @@
+
 #include <iostream>
 
 using namespace std;
-
+//TPC DO IRS
 int main() {
     float valorHora, horas;
     cout << "Valor/hora: "; cin >> valorHora;
@@ -17,5 +18,27 @@ int main() {
     cout << "Desconto IRS: " << descontoIRS << " euros" << endl;
     cout << "Sobra: " << sobra << " euros" << endl;
 
+    return 0;
+
+    //TPC DAS MEDIDAS
+}
+
+float pedido;
+int main()
+{
+    cout << "digite uma medida em metros que queria em milimetros: ";
+ cin >> pedido;
+ cout << "a medição em milimetros é: "<< pedido *1000 << endl;
+}
+
+//TPC DO RAIO
+float PI = 3.14159;
+
+int main() {
+    float raio;
+    cout << "Raio: "; 
+    cin >> raio;
+    cout << "Perimetro: " << 2 * PI * raio << endl;
+    cout << "Area: " << PI * raio * raio << endl;
     return 0;
 }
