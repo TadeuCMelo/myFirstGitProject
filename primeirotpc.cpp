fd1117a5f5e3a677a0794be2e115ff768757582a
TadeuCMelo <tadeumelo129@gmail.com>
2024-11-11 15:24:26 -0100

    awdsa

--- primeirotpc.cpp
+++ primeirotpc.cpp
@@ -13,3 +13,49 @@
 *e o desconto de 12.5% para o IRS.
 *
 */
+
+#include <iostream>
+
+using namespace std;
+//TPC DO IRS
+int main() {
+    system("clear");
+    float valorHora, horas;
+    cout << "Valor/hora: "; cin >> valorHora;
+    cout << "Horas: "; cin >> horas;
+
+    float total = valorHora * horas;
+    float descontoIRS = total * 0.125;
+    float sobra = total - descontoIRS;
+    
+    cout << endl;
+    
+    cout << "Total: " << total << " euros" << endl;
+    cout << "Desconto IRS: " << descontoIRS << " euros" << endl;
+    cout << "Sobra: " << sobra << " euros" << endl;
+
+    return 0;
+
+    //TPC DAS MEDIDAS
+}
+
+//float pedido;
+//int main()
+//{
+  //  cout << "digite uma medida em metros que queria em milimetros: ";
+ //cin >> pedido;
+ //cout << "a medição em milimetros é: "<< pedido *1000 << endl;
+//}
+
+//TPC DO RAIO
+//const float PI = 3.14159;
+
+//int main() {
+  //  float raio;
+    //cout << "Raio: "; 
+    //cin >> raio;
+    //cout << "Perimetro: " << 2 * PI * raio << endl;
+    //cout << "Area: " << PI * (raio * raio) << endl;
+    //cout << "diametro: " << 2* raio << endl;
+    //return 0;
+//}
