13574c9b8ef8e062eb9ef3ecbefe0aab96aa16a8
TadeuCMelo <tadeumelo129@gmail.com>
2024-12-04 00:17:42 -0100

    Update tipici.cpp

--- tipici.cpp
+++ tipici.cpp
@@ -1,38 +1,23 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
 using namespace std;
+int main(){
+    char letras[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
-int main() {
-    char letras[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     srand(time(0));
-    char letra_certa = letras[rand() % 26], chute;
-
-    cout << "Adivinha a letra (a-z)! Tens 4 tentativas." << endl;
-    for (int i = 0; i < 4; ++i) {
-        cout << "Tentativa " << i + 1 << ": ";
-        cin >> chute;
-
-        bool valido = false;
-        for (char c : letras) {
-            if (c == chute) {
-                valido = true;
-                break;
-            }
+    char letraEscolhida = letras[rand() % 26];
+    char palpite;
+    cout << "escreve uma letra de a-z: ";
+    do{
+        cin >> palpite;
+        if (palpite < letraEscolhida){
+            cout << "A letra é maior que " << palpite << ". Tente novamente: ";
+        } else if(palpite > letraEscolhida){
+            cout << "A letra é menor que " << palpite << ". Tente novamente: ";
+        } else{
+            cout << "Parabéns acertaste a letra: " << palpite << "!";
         }
-
-        if (!valido) {
-            cout << "É UMA LETRA MINÚSCULA! Tenta novamente." << endl;
-            --i;  
-        } else if (chute == letra_certa) { 
-            cout << "acertaste gostoso!" << endl;
-            return 0;  
-        } else { 
-            cout << "A letra é " << (chute < letra_certa ? "mais alta" : "mais baixa") << "." << endl;
-        }
-    }
-
-    cout << "Perdeste! A letra era '" << letra_certa << "'." << endl;
+    } while  (palpite != letraEscolhida);
     return 0;
 }
