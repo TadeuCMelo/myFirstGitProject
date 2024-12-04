d1bd6e671b2cda42fea8c3e026fb85579468ec1d
TadeuCMelo <tadeumelo129@gmail.com>
2024-12-04 00:21:14 -0100

    Update tipici.cpp

--- tipici.cpp
+++ tipici.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-using namespace std;
-int main(){
-    char letras[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
+using namespace std;
+
+int main() {
+    char alfabeto[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     srand(time(0));
-    char letraEscolhida = letras[rand() % 26];
-    char palpite;
-    cout << "escreve uma letra de a-z: ";
-    do{
-        cin >> palpite;
-        if (palpite < letraEscolhida){
-            cout << "A letra é maior que " << palpite << ". Tente novamente: ";
-        } else if(palpite > letraEscolhida){
-            cout << "A letra é menor que " << palpite << ". Tente novamente: ";
-        } else{
-            cout << "Parabéns acertaste a letra: " << palpite << "!";
+    char maquina = alfabeto[rand() % 26];
+    
+    char jogador;
+    for (int tentativas = 0; tentativas < 5; tentativas++) {
+        cout << "Tentativa " << tentativas + 1 << "\n"<< "escreve uma letra: ";
+        cin >> jogador;
+        
+        if (jogador == maquina) {
+            cout << "Parabéns acertaste!" << endl;
+            return 0;
         }
-    } while  (palpite != letraEscolhida);
+        cout << (jogador < maquina ? "Maior" : "Menor") << endl;
+    }
+    
+    cout << "Perdeste a letra era: " << maquina << endl;
     return 0;
 }
